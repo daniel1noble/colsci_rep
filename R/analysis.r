d41940ef1d611b6bf21f764ec3e0d158f0351eb6
Daniel Noble <daniel.wa.noble@gmail.com>
2014-11-04 17:37:25 +1100

    Updated the analysis script and created a knitR script so that it can be used with sublime. knit the ms.md file first then use pandoc ms.md -o ms.pdf to convert to the desired output file

--- R/analysis.r
+++ R/analysis.r
@@ -26,3 +26,45 @@ col_just   <- colnames(coldat)[c(grep("_just", colnames(coldat)))]
 coldat     <- coldat[,! colnames(coldat) %in% col_just][-c(2:13, 35:36)] # Additioally remove details on analysis and details on papers, but keep paper number.
 
 ## Things we need to check. The studies categorized as "multiple" Need to make clear what the values (0 or 1) correspond to camera or specs. 
+
+#--------------------------------------------- ANALYSIS -----------------------------------------#
+## How many studies utilized specs (1), cameras (2), both (3)?
+studytype <- table(coldat$stud_type)
+
+specs <- as.numeric(studytype[1])
+cam   <- as.numeric(studytype[2])
+both  <- as.numeric(studytype[3])
+
+## Of the studies using a spec, what proportion report the make and model?
+specModel <- (sum(coldat$spec_rep, na.rm = TRUE)/(specs+both))*100
+
+## Of the studies using a spec, what proportion report the make and model?
+camModel  <- (sum(coldat$cam_model, na.rm = TRUE)/(cam+both))*100
+
+## Of the studies using a spec, what proportion report the light source. Note that column needs to be extracted because it combines cameras and specs
+
+DatSpec   <- subset(coldat, stud_type == 1 |stud_type == 3)
+specLight <- (sum(DatSpec$light_source)/nrow(DatSpec))*100
+
+## Of the studies using a cam, what proportion report the light source. Note that column needs to be extracted because it combines cameras and specs
+
+DatCam    <- subset(coldat, stud_type == 2 |stud_type == 3)
+CamLight  <- (sum(DatCam$light_source, na.rm = TRUE)/nrow(DatCam))*100
+
+## Of Studies spec'ing what proportion report a dark standard
+drk_std   <- (sum(DatSpec$dark_std)/(nrow(DatSpec)))*100
+#white_std <- sum(DatSpec$white_std)/(nrow(DatSpec))
+
+## What proportion of studies specing report the num of specs averaged
+
+specAVG <- (sum(DatSpec$specpix_avg)/nrow(DatSpec))*100
+
+## What proportion of studies using a camera report the num of pixs averaged
+
+camAVG <- (sum(DatCam$specpix_avg)/nrow(DatCam))*100
+
+## What proportion of spec studies reported 1) the integration time, spec angle and distance?
+
+intTime <- (sum(DatSpec$int_time)/nrow(DatSpec))*100
+angle   <- (sum(DatSpec$spec_angle)/nrow(DatSpec))*100
+dist    <- (sum(DatSpec$spec_dist)/nrow(DatSpec))*100
--- R/knit_doc.r
+++ R/knit_doc.r
@@ -1,6 +1,11 @@
 
-#Read necessary functions and files
+packages <- c("knitr", "markdown")
+install.packages(packages)
+sapply(packages, function(x) library(x, character.only = TRUE))
+
+#Read necessary functions and files. There are two functions knit2pdf and knit2html which will produce documents directly, but there are problems with pdf and also problems rendering the markdown file when complete. Best to just use knit first then take md file and convert in ternimal. 
+
 rm(list=ls())
 setwd("./docs")
-knit2pdf("ms.Rmd")
+knit("ms.Rmd")
 setwd("..")
