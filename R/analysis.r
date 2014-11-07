2019f7d9f46c490acc90fc4eccf68310dee6efdb
Daniel Noble <daniel.wa.noble@gmail.com>
2014-11-07 14:39:17 +1100

    Added new analysis script

--- R/analysis.r
+++ R/analysis.r
@@ -46,30 +46,30 @@ camModel  <- (sum(coldat$cam_model, na.rm = TRUE)/(cam+both))*100
 DatSpec   <- subset(coldat, stud_type == 1 |stud_type == 3)
 
 # Note that we need to change the rows where spec and cemera differ
-specLight <- (sum(DatSpec$light_source)/nrow(DatSpec))*100
+specLight <- (sum(DatSpec$light_source)/length(DatSpec$light_source))*100
 
 ## Of the studies using a cam, what proportion report the light source. Note that column needs to be extracted because it combines cameras and specs
 
 DatCam    <- subset(coldat, stud_type == 2 |stud_type == 3)
-CamLight  <- (sum(DatCam$light_source, na.rm = TRUE)/nrow(DatCam))*100
+CamLight  <- (sum(DatCam$light_source, na.rm = TRUE)/length(DatCam$light_source))*100
 
-## Of Studies spec'ing what proportion report a dark standard
-drk_std   <- (sum(DatSpec$dark_std)/(nrow(DatSpec)))*100
-white_stdspec <- (sum(DatSpec$white_stdspec)/nrow(DatSpec))*100
+## Of studies spec'ing what proportion report a dark standard
+drk_std       <- (sum(DatSpec$dark_std)/(length(DatSpec$dark_std)))*100
+white_stdspec <- (sum(DatSpec$white_stdspec)/length(DatSpec$white_stdspec))*100
 
 ## What proportion of studies specing report the num of specs averaged
 
-specAVG <- (sum(DatSpec$specpix_avg)/nrow(DatSpec))*100
+specAVG <- (sum(DatSpec$specpix_avg)/length(DatSpec$specpix_avg))*100
 
 ## What proportion of studies using a camera report the num of pixs averaged
 
-camAVG <- (sum(DatCam$specpix_avg)/nrow(DatCam))*100
+camAVG  <- (sum(DatCam$specpix_avg)/length(DatCam$specpix_avg))*100
 
+##--------------------------------------
 ## How do studies analyse colour?
 analysisType <- table(coldat$analysis_type)
 
 ## How about just spec studies because they can do visual modelling, or colorimetric
-
 analysisTypeSpec <- table(DatSpec$analysis_type)
 
 ## What proportion of spec studies reported 1) the integration time, spec angle and distance?
@@ -83,5 +83,17 @@ dist    <- (sum(DatSpec$spec_dist)/nrow(DatSpec))*100
 col_def <- sum(coldat$colmetric_def, na.rm = TRUE)/(length(coldat$colmetric_def[!is.na(coldat$colmetric_def)]))
 
 ## How did studies fair in their reporting of visual models?
+prop_irrad <- (sum(DatSpec$irrad_type[! is.na(DatSpec$irrad_type)])/length(DatSpec$irrad_type[! is.na(DatSpec$irrad_type)]))*100 # Note that there are a few studies under "multiple" that need to be checked because irrad_type is NA and yet the visual model they report is not given. We should check these.
 
-DatSpec
+prop_vismod <- (sum(DatSpec$vis_mod[! is.na(DatSpec$vis_mod)])/length(DatSpec$vis_mod[! is.na(DatSpec$vis_mod)]))*100
+
+prop_spvismod <- (sum(DatSpec$vis_mod_sp[! is.na(DatSpec$vis_mod_sp)])/length(DatSpec$vis_mod_sp[! is.na(DatSpec$vis_mod_sp)]))*100
+
+propvis_mod_param <- (sum(DatSpec$vis_mod_param[! is.na(DatSpec$vis_mod_param)])/length(DatSpec$vis_mod_param[! is.na(DatSpec$vis_mod_param)]))*100
+
+
+##------------------------------------ Figure 1--------------------------------------##
+# Figure of proportions on the hardware/software used
+
+props   <- c(specModel, camModel, specLight, CamLight, drk_std, white_stdspec, specAVG, camAVG)
+N_props <- c((specs+both), (cam+both), length(DatSpec$light_source), length(DatCam$light_source), )
