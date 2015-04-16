5e7b13731f6480f72152f5f000f8efe50d5624ef
Daniel Noble <daniel.wa.noble@gmail.com>
2015-04-16 15:03:18 +1000

    Added source code to run analysis before kniting doc

--- R/knit_doc.r
+++ R/knit_doc.r
@@ -1,5 +1,10 @@
 #Read necessary functions and files. There are two functions knit2pdf and knit2html which will produce documents directly, but there are problems with pdf and also problems rendering the markdown file when complete. Best to just use knit first then take md file and convert in ternimal. 
 
-setwd("./docs")
+# First source analysis file 
+source("R/analysis.R")
+
+# knit the .RmD file with R code
+
+setwd("./docs/")
 knit("ms.Rmd")
 setwd("..")
